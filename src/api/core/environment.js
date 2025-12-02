98d3d6009fadb88af59fac2eb4a56a5026a03302
setongee <setongee@gmail.com>
2025-12-02 09:23:15 +0100

    mepb

--- src/api/core/environment.js
+++ src/api/core/environment.js
@@ -1,5 +1,7 @@
-const environment = { test : "http://localhost:8000/api/v2", live : "https://oyster-app-7x7p7.ondigitalocean.app/api/v2" }
+const environment = {
+  test: 'http://localhost:8083/api/v2',
+  live: 'https://web3.lagosstate.gov.ng/api/v2',
+};
+const env = environment.live;
 
-const env = environment.live
-
-export { env }
+export { env };
